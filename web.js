8d9d7db016b0e6e8cd666f7816652bc99d05b144
siju100 <siju100@gmail.com>
2013-07-10 05:25:19 +0000

    web.js updated:

--- web.js
+++ web.js
@@ -1,12 +1,8 @@
+var fs = require('fs');
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var buf = fs.readFileSync('index.html','utf-8');
+var string = buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-console.log("Listening on "+ port);
+  response.send(string);
 });
